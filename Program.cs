98398ac03dd7a9a8e12992e5b7fbccdd428b2412
vincentcetrone <98615051+vincentcetrone@users.noreply.github.com>
2022-02-07 11:04:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = GetDayOfWeek();
+            BirthdayDecision(day);
+        }
+        static string GetDayOfWeek() 
+        {
+            Console.WriteLine(" What day of the week is it?");
+            string day = Console.ReadLine();
+
+            return day;
+        }
+
+        static void BirthdayDecision(String day)
+        {
+            string name;
+
+            if (day == "Monday")
+            {
+                name = "Joseph";
+                SayHappyBirthday(name);
+            }
+            else if(day == "Thursday")
+            {
+                name = "katie";
+                SayHappyBirthday(name);
+            }
+            else
+            {
+                Console.WriteLine("No birthdays today :(");
+            }
+        }
+        static void SayHappyBirthday(string name)
+        {
+            Console.WriteLine("Happy Birthday"+name);
+        }
+    }
+}
+            
+
+            
+        
+
+
+
+
+
+            
+
+  
+                
+            
+            
+             
+    
+    
+
+
+
+
+
+
+
+    
+
+            
+            
+        
+        
+        
+         
+        
+            
+        
+
